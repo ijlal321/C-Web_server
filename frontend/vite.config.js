c5a43ac36ad8122d1d46633cefc6e8efbb1cfae8
ijlal123 <ijlal.tanveer291@gmail.com>
2025-09-17 15:42:50 +0500

    fized vite proxy bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    '/': 'http://localhost:8000',  // just for development. For hot reloading.
-  },
   server: {
+    proxy: {
+      "/download_chunk": {
+        target: "http://localhost:8080", // for dev only, for hot reloading.
+        changeOrigin: true
+      }
+    },
     port: 4000,
     host: true, // Allow access from other devices on the same network
   }
